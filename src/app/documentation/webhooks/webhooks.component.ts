b1e6678d1ea862f9c63b555d84d09b18121b830b
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2019-12-25 23:52:06 +0100

    webhooks docs page updated

--- src/app/documentation/webhooks/webhooks.component.ts
+++ src/app/documentation/webhooks/webhooks.component.ts
@@ -12,7 +12,7 @@ export class WebhooksComponent implements OnInit {
     data: {
       id: 'eb5b3138ff0dbcb060eb111b7609d01d',
       appId: '84U0IRP0rdC57AHfwRQc',
-      userId: 'pwBBKwhhVXJ16xtEcgKA',
+      accountId: 'pwBBKwhhVXJ16xtEcgKA',
       blockHeight: 2104164,
       paymentId: '6a8db2c83a34f29275d3cfad7100944168c46fd5d43e074aa038e18a0410c7dd',
       depositAddress: 'TRTLuxVTfpvXTXbsQxzqy5KHyHXTQZbtEHnAsorKPqkweHzDRcRyM28g6jJMQjBoocYqsjtR3G5n1ipuDpn6VbqZQWnQ198HZcD',
@@ -21,7 +21,7 @@ export class WebhooksComponent implements OnInit {
       txHash: 'e392965de03d3553df994baffba2bbb027ec83c947c4ddec9d6791cc86bca588',
       createdDate: 1576336806682,
       status: 'confirming',
-      userCredited: false,
+      accountCredited: false,
       lastUpdate: 1576336806682,
       cancelled: false
     }
@@ -42,8 +42,8 @@ export class WebhooksComponent implements OnInit {
       paymentId: '6a8db2c83a34f29275d3cfad7100944168c46fd5d43e074aa038e18a0410c7dd',
       status: 'completed',
       txHash: 'e392965de03d3553df994baffba2bbb027ec83c947c4ddec9d6791cc86bca588',
-      userCredited: true,
-      userId: 'pwBBKwhhVXJ16xtEcgKA'
+      accountCredited: true,
+      accountId: 'pwBBKwhhVXJ16xtEcgKA'
     }
   };
 
@@ -61,8 +61,8 @@ export class WebhooksComponent implements OnInit {
       lastUpdate: 1576337042609,
       paymentId: '6a8db2c83a34f29275d3cfad7100944168c46fd5d43e074aa038e18a0410c7dd',
       status: 'completed',
-      userCredited: false,
-      userId: 'pwBBKwhhVXJ16xtEcgKA'
+      accountCredited: false,
+      accountId: 'pwBBKwhhVXJ16xtEcgKA'
     }
   };
 
@@ -82,7 +82,7 @@ export class WebhooksComponent implements OnInit {
       status: 'completed',
       timestamp: 1576340903981,
       txHash: '07e8f4ee5a0dcdf3ca3ce987069f107d045def181d438696114fb6990fb3c72c',
-      userId: 'jaKrijd8WjHRWTu2y8pG'
+      accountId: 'jaKrijd8WjHRWTu2y8pG'
     }
   };
 
@@ -101,23 +101,7 @@ export class WebhooksComponent implements OnInit {
       requestedAtBlock: 2104300,
       status: 'completed',
       timestamp: 1576340903981,
-      userId: 'jaKrijd8WjHRWTu2y8pG'
-    }
-  };
-
-  userUpdated = {
-    code: 'user/updated',
-    data: {
-      appId: '84U0IRP0rdC57AHfwRQc',
-      balanceLocked: 0,
-      balanceUnlocked: 5,
-      createdAt: 1576008872016,
-      deleted: false,
-      depositAddress: 'TRTLuxsFnkbHRdfoKCFc8KJ6Rzvp1TdqLHdJctadbENjAZx5mhgXqQXA6kNfcuNnap94kdBUwnfvPHvh82YufKFmHbXLoKCzfHLXTXbsQxzqy5KHyHXTQZbtEHnAsorKPqkweHzDRcRyM28g6jJMQjBoocYqsjtR3G5n1ipuDpn6VbqZQWnQ1D22qvB',
-      depositQrCode: 'https://chart.googleapis.com/chart?cht=qr&chs=256x256&chl=turtlecoin://TRTLuxsFnkbHRdfoKCFc8KJ6Rzvp1TdqLHdJctadbENjAZx5mhgXqQXA6kNfcuNnap94kdBUwnfvPHvh82YufKFmHbXLoKCzfHLXTXbsQxzqy5KHyHXTQZbtEHnAsorKPqkweHzDRcRyM28g6jJMQjBoocYqsjtR3G5n1ipuDpn6VbqZQWnQ1D22qvB',
-      paymentId: '6a8db2c83a34f29275d3cfad7100944168c46fd5d43e074aa038e18a0410c7dd',
-      userId: 'pwBBKwhhVXJ16xtEcgKA',
-      withdrawAddress: 'TRTLv32bGBP2cfM3SdijU4TTYnCPoR33g5eTas6n9HamBvu8ozc9BZHWza5j7cmBFSgh4dmmGRongfoEEzcvuAEF8dLxixsS7he'
+      accountId: 'jaKrijd8WjHRWTu2y8pG'
     }
   };
 
