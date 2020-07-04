8b9abaa52a7e3d95919f82ebfaa76ef0f562cfda
Jacques Jordaan <5298218+zoidbergZA@users.noreply.github.com>
2020-07-04 13:43:09 +0200

    docs navlink updated to website

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
 export const Constants = {
-  trtlAppsNpmLink: 'https://www.npmjs.com/package/trtl-apps'
+  trtlAppsNpmLink: 'https://www.npmjs.com/package/trtl-apps',
+  docsAppsUrl: 'https://www.trtlapps.com/docs/apps/overview'
 }
